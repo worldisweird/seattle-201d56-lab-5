2cc54be58b9df0069e5f2a24cc27c14c59832398
Sudip Adhikari <sudeep_sudeep07@hotmail.com>
2019-03-29 16:57:16 -0700

    lab5 succesful for 5 problems

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -99,7 +99,6 @@ function sumArray(sumArr) { //eslint-disable-line
 }
 
 // Here is the test for sumArray(); uncomment it to run it
-
 testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -116,11 +115,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  var product3=[];
+  var product2= multiply(multArr[0], multArr[1])[0];
+  var multiply3= multiply(product2, multArr[2])[0];
+  product3.push(multiply3);
+  product3.push('The numbers ' + multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + multiply3 + '.');
+  return product3;
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
