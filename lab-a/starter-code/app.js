c69923a5fb1bd4b0f8137ff2e6008b564084838c
Sudip Adhikari <sudeep_sudeep07@hotmail.com>
2019-03-29 14:36:53 -0700

    multiplication and addition complete

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -32,11 +32,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var outputMultiply=[];
+  var multiplyNumbers= a * b;
+  outputMultiply.push(multiplyNumbers);
+  outputMultiply.push('The product of ' + a + ' and ' + b + ' is ' + multiplyNumbers + '.');
+  return outputMultiply;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
